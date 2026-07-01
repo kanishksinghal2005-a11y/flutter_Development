43dff82cab620f02e4e1db5e8847e7132633487c
Kanishk Singhal <kanishksinghal2005@gmail.com>
2026-07-01 14:48:36 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,196 +1,32 @@
-// // import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+// import "package:fluppy/screens/align_widget.dart";
+// import "package:fluppy/screens/container_widget.dart";
+// import "package:fluppy/screens/row_column_widgets.dart";
+// import "package:fluppy/screens/safearea_widget.dart";
+// import "package:fluppy/screens/spacer.dart";
+// import "package:fluppy/screens/stack_widget.dart";
+// import "package:fluppy/screens/stateful_widget.dart";
+// import "package:fluppy/screens/stateless_widget.dart";
+import "package:fluppy/screens/wrap_widget.dart";
 
-// // void main() {
-// //   runApp(const MyApp());
-// // }
-
-// // class MyApp extends StatelessWidget {
-// //   const MyApp({super.key});
-
-// //   // This widget is the root of your application.
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return MaterialApp(
-// //       title: 'Flutter Demo',
-// //       theme: ThemeData(
-// //         // This is the theme of your application.
-// //         //
-// //         // TRY THIS: Try running your application with "flutter run". You'll see
-// //         // the application has a purple toolbar. Then, without quitting the app,
-// //         // try changing the seedColor in the colorScheme below to Colors.green
-// //         // and then invoke "hot reload" (save your changes or press the "hot
-// //         // reload" button in a Flutter-supported IDE, or press "r" if you used
-// //         // the command line to start the app).
-// //         //
-// //         // Notice that the counter didn't reset back to zero; the application
-// //         // state is not lost during the reload. To reset the state, use hot
-// //         // restart instead.
-// //         //
-// //         // This works for code too, not just values: Most code changes can be
-// //         // tested with just a hot reload.
-// //         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-// //       ),
-// //       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-// //     );
-// //   }
-// // }
-
-// // class MyHomePage extends StatefulWidget {
-// //   const MyHomePage({super.key, required this.title});
-
-// //   // This widget is the home page of your application. It is stateful, meaning
-// //   // that it has a State object (defined below) that contains fields that affect
-// //   // how it looks.
-
-// //   // This class is the configuration for the state. It holds the values (in this
-// //   // case the title) provided by the parent (in this case the App widget) and
-// //   // used by the build method of the State. Fields in a Widget subclass are
-// //   // always marked "final".
-
-// //   final String title;
-
-// //   @override
-// //   State<MyHomePage> createState() => _MyHomePageState();
-// // }
-
-// // class _MyHomePageState extends State<MyHomePage> {
-// //   int _counter = 0;
-
-// //   void _incrementCounter() {
-// //     setState(() {
-// //       // This call to setState tells the Flutter framework that something has
-// //       // changed in this State, which causes it to rerun the build method below
-// //       // so that the display can reflect the updated values. If we changed
-// //       // _counter without calling setState(), then the build method would not be
-// //       // called again, and so nothing would appear to happen.
-// //       _counter++;
-// //     });
-// //   }
-
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     // This method is rerun every time setState is called, for instance as done
-// //     // by the _incrementCounter method above.
-// //     //
-// //     // The Flutter framework has been optimized to make rerunning build methods
-// //     // fast, so that you can just rebuild anything that needs updating rather
-// //     // than having to individually change instances of widgets.
-// //     return Scaffold(
-// //       appBar: AppBar(
-// //         // TRY THIS: Try changing the color here to a specific color (to
-// //         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-// //         // change color while the other colors stay the same.
-// //         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-// //         // Here we take the value from the MyHomePage object that was created by
-// //         // the App.build method, and use it to set our appbar title.
-// //         title: Text(widget.title),
-// //       ),
-// //       body: Center(
-// //         // Center is a layout widget. It takes a single child and positions it
-// //         // in the middle of the parent.
-// //         child: Column(
-// //           // Column is also a layout widget. It takes a list of children and
-// //           // arranges them vertically. By default, it sizes itself to fit its
-// //           // children horizontally, and tries to be as tall as its parent.
-// //           //
-// //           // Column has various properties to control how it sizes itself and
-// //           // how it positions its children. Here we use mainAxisAlignment to
-// //           // center the children vertically; the main axis here is the vertical
-// //           // axis because Columns are vertical (the cross axis would be
-// //           // horizontal).
-// //           //
-// //           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-// //           // action in the IDE, or press "p" in the console), to see the
-// //           // wireframe for each widget.
-// //           mainAxisAlignment: .center,
-// //           children: [
-// //             const Text('You have pushed the button this many times:'),
-// //             Text(
-// //               '$_counter',
-// //               style: Theme.of(context).textTheme.headlineMedium,
-// //             ),
-// //           ],
-// //         ),
-// //       ),
-// //       floatingActionButton: FloatingActionButton(
-// //         onPressed: _incrementCounter,
-// //         tooltip: 'Increment',
-// //         child: const Icon(Icons.add),
-// //       ),
-// //     );
-// //   }
-// // }
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: Colors.blue,
-//           title: Text('Student ID Card'),
-//           centerTitle: false,
-//         ),
-//         body: Center(
-//           child: Text(
-//             'Hi welcome to First flutter project',
-//             style: TextStyle(
-//               color: Colors.white,
-//             ),
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
- import 'package:flutter/material.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar:AppBar(backgroundColor: Colors.blue,title:Text("Student ID Card"),centerTitle: false,),
-        body: Center(
-          child: Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey,
-                width: 2,
-              ),
-            ),
-              child:Column(
-          mainAxisAlignment: MainAxisAlignment.center,children: [Icon(Icons.person,size: 50,),SizedBox(height:10),Text("Kanishk Singhal",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),SizedBox(height:10),Text("Btech CSE", style: TextStyle(fontSize: 15),),SizedBox(height:10),Text("Roll No : 2415000765",style: TextStyle(fontSize: 15),)],
-            ),
-          ),
-        ),
-        
-        )
-      ),
-    
-  );
+void main(){
+  runApp(MyApp());
 }
 
-// import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-// void main(){
-//   runApp(MyApp());
-// }
-
-
-// class MyApp extends StatelessWidget
-// {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home :HomeScreen()
-//     );
-//   }
-
-// }
-
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home:ContainerWidget()
+      // home:SafeAreaWidget()
+      // home:RowColumnWidgets()
+      // home:StackWidget()
+      // home:SpacerWidget()
+      // home:AlignWidget()
+      home:WrapWidget()
+    );
+  }
+}
